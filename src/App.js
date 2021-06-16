09b94dbb76062a3c338e7400dfdb2ffe08ed6480
Vishnu Mohan <vishnumohan1996nov@gmail.com>
2021-06-16 20:59:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useRef, useEffect, useState } from "react";
+import './index.css'
 function App() {
+  const videoRef = useRef(null);
+  const photoRef = useRef(null);
+  const [hasPhoto, setHasPhoto] = useState(false);
+  const getVideo = () => {
+    navigator.mediaDevices.getUserMedia({
+      video: { width: 1920, height: 1080 },
+    }).then((stream)=>{
+      let video = videoRef.current;
+      video.srcObject = stream;
+      video.play();
+    })
+    .catch(err=>console.log(err))
+  };
+
+  const takePhoto = ()=>{
+    const width = 414;
+    const height = width / (16/9)
+    let video = videoRef.current
+    let photo = photoRef.current
+    photo.width = width;
+    photo.height = height
+    let ctx = photo.getContext('2d')
+    ctx.drawImage(video, 0, 0, width, height)
+    setHasPhoto(true)
+  }
+
+  const closePhoto = ()=>{
+    let photo = photoRef.current
+    let ctx = photo.getContext('2d')
+    ctx.clearRect(0, 0, photo.width, photo.height)
+    setHasPhoto(false)
+  }
+
+  useEffect(()=>{
+    getVideo();
+  },[videoRef])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="camera">
+        <video ref={videoRef}></video>
+        <button onClick={takePhoto}>SNAP</button>
+      </div>
+      <div className={'result ' + (hasPhoto ? "hasPhoto" : "")}>
+        <canvas ref={photoRef}></canvas>
+        <button onClick={closePhoto}>Close</button>
+      </div>
     </div>
   );
 }
